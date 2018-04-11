794ce04c7fa21519da9a71849b2d013db30eb1c5
Hardy Har <hardyhar@email.com>
2018-04-11 13:47:04 -0500

    Added initial

---
+++ src/main/java/com/jeff/ThymeleafApplication.java
@@ -0,0 +1,28 @@
+package com.jeff;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Arrays;
+import java.util.List;
+
+@Controller
+@SpringBootApplication
+public class ThymeleafApplication {
+
+	@RequestMapping("/")
+	public String home(Model model) {
+		model.addAttribute("author","Jethrow");
+		model.addAttribute("height","6'2");
+		List favoriteFood = Arrays.asList("pizza","tacos","ice cream");
+		model.addAttribute("favoriteFood",favoriteFood);
+		return "index.html";
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThymeleafApplication.class, args);
+	}
+}
